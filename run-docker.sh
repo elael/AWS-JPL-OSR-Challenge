8626630e2ca25b8305550b73cbe68f3939e0b21b
Joe Barbere <joe.barbere@gmail.com>
2020-02-22 12:21:51 -0500

    Adding run-docker scripts

---
+++ run-docker.sh
@@ -0,0 +1,9 @@
+docker run  -p 5900:5900 \
+            -p 9000:9000 \
+            -p 6080:80 \
+            -p 5601:5601 \
+            -p 8888:8888 \
+            -v /home/joebarbere/dockervolumes/awsjpl:/data \
+            -v /home/joebarbere/GitHub/AWS-JPL-OSR-Challenge:/home/ubuntu/catkin_ws/src \
+            -v /home/joebarbere/dockervolumes/awsjpl-gazebo:/root/.gazebo \
+            awsjpl
